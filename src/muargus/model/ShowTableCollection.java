766fc1cf9b3a50de40b98cb2e600c2f0b5834e5b
ambargus <ambargus@NBAMBARGUS>
2014-09-02 11:35:05 +0200

    begin ShowTableCollection gemaakt

---
+++ src/muargus/model/ShowTableCollection.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package muargus.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ambargus
+ */
+public class ShowTableCollection {
+
+    
+    private ArrayList<VariableMu> variables;
+    
+    public ShowTableCollection() {
+    }
+    
+    public void setVariables(ArrayList<VariableMu> variables){
+        this.variables = variables;
+    }
+    
+    
+    
+}
