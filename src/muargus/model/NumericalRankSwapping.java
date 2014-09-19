19b4d4f2f68a272f02d5b29970bfd6c785ee6c28
ambargus <ambargus@NBAMBARGUS>
2014-09-19 11:52:04 +0200

    javadocs NumericalRankSwapping

--- src/muargus/model/NumericalRankSwapping.java
+++ src/muargus/model/NumericalRankSwapping.java
@@ -1,31 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package muargus.model;
 
 import java.util.ArrayList;
 
 /**
+ * Model class of the NumericalRankSwapping screen. Only a single instance of
+ * this class will exist.
  *
- * @author ambargus
+ * @author Statistics Netherlands
  */
 public class NumericalRankSwapping {
+
     private final ArrayList<VariableMu> variables;
     private final ArrayList<RankSwappingSpec> rankSwappings;
-    
+
+    /**
+     * Constructor of the model class NumericalRankSwapping. Makes empty
+     * arraylists for the variables and the rankSwappings.
+     */
     public NumericalRankSwapping() {
         this.variables = new ArrayList<>();
         this.rankSwappings = new ArrayList<>();
     }
 
+    /**
+     * Gets the ArrayList containing all the numerical variables. If the
+     * ArrayList is empty, use this method to add VariableMu's.
+     *
+     * @return ArrayList containing all the numerical variables.
+     */
     public ArrayList<VariableMu> getVariables() {
         return this.variables;
     }
 
+    /**
+     * Gets the ArrayList containing the variable specifications for rank
+     * swapping. The RankSwappingSpec contains all relevant information for
+     * applying rank swapping on the relevant variable. If the ArrayList is
+     * empty, use this method to add RankSwappingSpec's.
+     *
+     * @return ArrayList containing RankSwappingSpec's.
+     */
     public ArrayList<RankSwappingSpec> getRankSwappings() {
         return this.rankSwappings;
-    }    
+    }
 }
