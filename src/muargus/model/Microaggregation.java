1796d4a2711ffb25e9e57f5036595d331a8b1282
ambargus <ambargus@NBAMBARGUS>
2014-09-19 11:48:20 +0200

    javadocs MicroAggregation

--- src/muargus/model/Microaggregation.java
+++ src/muargus/model/Microaggregation.java
@@ -3,30 +3,49 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package muargus.model;
 
 import java.util.ArrayList;
 
 /**
+ * Model class of the Microaggregation screen. Only a single instance of this
+ * class will exist.
  *
- * @author ambargus
+ * @author Statistics Netherlands
  */
 public class Microaggregation {
+
     private final ArrayList<VariableMu> variables;
     private final ArrayList<MicroaggregationSpec> microaggregations;
-    
+
+    /**
+     * Constructor of the model class Microaggregation. Makes empty arraylists
+     * for the variables and the microaggregations.
+     */
     public Microaggregation() {
         this.microaggregations = new ArrayList<>();
         this.variables = new ArrayList<>();
     }
-            
+
+    /**
+     * Gets the ArrayList containing all the numerical variables.
+     *
+     * @return ArrayList containing all the numerical variables.
+     */
     public ArrayList<VariableMu> getVariables() {
         return this.variables;
     }
-    
+
+    /**
+     * Gets the ArrayList containing the variable specifications for the
+     * microaggregation. The MicroaggregationSpec contains all relevant
+     * information for applying microaggreation on the relevant variable. If the
+     * ArrayList is empty, use this method to add MicroaggregationSpec's.
+     *
+     * @return ArrayList containing MicroaggregationSpec's.
+     */
     public ArrayList<MicroaggregationSpec> getMicroaggregations() {
         return microaggregations;
-    }   
-    
+    }
+
 }
