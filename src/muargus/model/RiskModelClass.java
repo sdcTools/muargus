23787f72ef9d2132c300549dc03a103b04463b05
ambargus <ambargus@NBAMBARGUS>
2014-09-19 15:38:17 +0200

    javadocs RiskModelClass

--- src/muargus/model/RiskModelClass.java
+++ src/muargus/model/RiskModelClass.java
@@ -1,21 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package muargus.model;
 
 /**
+ * Model class of the RiskSpecification screen (both individual and household).
+ * Only a single instance of this class will exist.
  *
- * @author pibd05
+ * @author Statistics Netherlands
  */
 public class RiskModelClass {
+
     private final double leftValue;
     private final double rightValue;
     private final int frequency;
     private final int hhFrequency;
-    
+
+    /**
+     * Constructor of the model class RiskModelClass.
+     *
+     * @param leftValue Double containing the bottom left hand value.
+     * @param rightValue Double containing the upper right hand value.
+     * @param frequency Integer containing the individual risk frequency.
+     * @param hhFrequency Integer containing the household frequency.
+     */
     public RiskModelClass(double leftValue, double rightValue, int frequency, int hhFrequency) {
         this.leftValue = leftValue;
         this.rightValue = rightValue;
@@ -23,22 +28,40 @@ public class RiskModelClass {
         this.hhFrequency = hhFrequency;
     }
 
+    /**
+     * Gets the bottom left hand value.
+     *
+     * @return Double containing the bottom left hand value.
+     */
     public double getLeftValue() {
         return this.leftValue;
     }
 
+    /**
+     * Gets the upper right hand value.
+     *
+     * @return Double containing the upper right hand value.
+     */
     public double getRightValue() {
         return this.rightValue;
     }
 
+    /**
+     * Gets the individual risk frequency.
+     *
+     * @return Integer containing the individual risk frequency.
+     */
     public int getFrequency() {
         return this.frequency;
     }
 
+    /**
+     * Gets the household frequency.
+     *
+     * @return Integer containing the household frequency.
+     */
     public int getHhFrequency() {
         return this.hhFrequency;
     }
-    
-    
-    
+
 }
