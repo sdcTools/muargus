00bbe8a5b00d09fe962ba0031c07759380942c68
ambargus <ambargus@NBAMBARGUS>
2014-09-19 15:31:12 +0200

    javadocs ReplacementFile en ReplacementSpec

--- src/muargus/model/ReplacementFile.java
+++ src/muargus/model/ReplacementFile.java
@@ -1,55 +1,73 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package muargus.model;
 
 import argus.model.ArgusException;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 
 /**
+ * Model class of the replacement file.
  *
- * @author pibd05
+ * @author Statistics Netherlands
  */
 public class ReplacementFile {
+
     private final File inputFile;
     private final File outputFile;
-    private final String replacementType;
-    
+    private final String replacementType; //TODO: wordt dit gebruikt? Kan het ook een int zijn?
+
+    /**
+     * Constructor of the model class ReplacementFile.
+     *
+     * @param replacementType String containing the replacement type.
+     * @throws ArgusException Throws an ArgusException when the replacement file
+     * cannot be created.
+     */
     public ReplacementFile(String replacementType) throws ArgusException {
         this.replacementType = replacementType;
         this.inputFile = createFile();
         this.outputFile = createFile();
     }
 
+    /**
+     * Gets the input file path.
+     *
+     * @return String containing the input file path.
+     */
     public String getInputFilePath() {
         return this.inputFile.getPath();
     }
 
+    /**
+     * Gets the output file path.
+     *
+     * @return String containing the output file path.
+     */
     public String getOutputFilePath() {
         return this.outputFile.getPath();
     }
 
+    /**
+     * Gets the replacement type.
+     *
+     * @return String containing the replacement type.
+     */
     public String getReplacementType() {
         return replacementType;
     }
-    
+
+    /**
+     * Creates a replacement file.
+     *
+     * @return File instance containing the replacement file.
+     * @throws ArgusException Throws an ArgusException when the replacement file
+     * cannot be created.
+     */
     private File createFile() throws ArgusException {
         try {
             return File.createTempFile("MuArgus", ".rpl");
-        }
-        catch (IOException ex) {
+        } catch (IOException ex) {
             throw new ArgusException("Replacement file cannot be created: " + ex.getMessage());
         }
     }
-    
-    
-    
-    
-    
-    
+
 }
